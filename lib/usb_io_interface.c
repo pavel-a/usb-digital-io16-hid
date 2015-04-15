b3f9ae680a7ccffd87cdbfa0de939185e365a988
pavel <pavel@aaa>
2015-04-15 23:25:19 +0300

    Impl. LED on/off

--- lib/usb_io_interface.c
+++ lib/usb_io_interface.c
@@ -95,19 +95,18 @@ static const char *usbErrorMessage(int errCode)
     return buffer;
 }
 
-//
-//
-static int rel_read_status_raw(USBDEVHANDLE dev, void *raw_data)
+
+// Read HID report
+static int d16_read_rep(USBDEVHANDLE dev, int reportnum, unsigned char raw_data[8+1])
 {
     char buffer[10];
     int err;
-    int reportnum = 0;
     int len = 8 + 1; /* report id 1 byte + 8 bytes data */
     memset(buffer, 0, sizeof(buffer));
 
     err = usbhidGetReport(dev, reportnum, buffer, &len);
     if ( err ) {
-        printerr("error reading status: %s\n", usbErrorMessage(err));
+        printerr("error reading hid report: %s\n", usbErrorMessage(err));
         return -1;
     }
 
@@ -121,13 +120,29 @@ static int rel_read_status_raw(USBDEVHANDLE dev, void *raw_data)
         memcpy(raw_data, buffer, len);
     }
 
-    return (unsigned char)buffer[8]; /* byte of relay states */
+    return 0;
 }
 
 
+// Write HID report
+static int d16_write_rep(USBDEVHANDLE dev, int reportnum, unsigned char buffer[8+1])
+{
+    int err;
+    int len = 8 + 1; /* report id 1 byte + 8 bytes data */
+
+    buffer[0] = (unsigned char)reportnum;
+    err = usbhidSetReport(dev, buffer, len);
+    if ( err ) {
+        printerr("error writing hid report: %s\n", usbErrorMessage(err));
+        return -1;
+    }
+
+    return err;
+}
+
 // Enum function for building list of devices
 static
-    int enumfunc(USBDEVHANDLE usbh, void *context)
+int enumfunc(USBDEVHANDLE usbh, void *context)
 {
     static const char vendorName[] = USB_IO16_VENDOR_NAME;
     static const char productName[] = USB_IO16_NAME;
@@ -160,7 +175,7 @@ static
     }
 
     /* Check the unique ID: USB_IO16_ID_STR_LEN  bytes at offset 1 (just after the report id) */
-    err = rel_read_status_raw(usbh, buffer);
+    err = d16_read_rep(usbh, 0, buffer);
     if ( err < 0 )
     {
         dbgprintf("Error reading report 0: %s\n", usbErrorMessage(err));
@@ -208,7 +223,63 @@ next:
     return 1;
 }
 
-#if 0
+
+// Public functions:
+
+/** Initialize the Library
+@returns: This function returns 0 on success and -1 on error.
+*/
+int EXPORT_API usb_io_init(void)
+{
+    return 0;
+}
+
+/** Finalize the Library.
+This function frees all of the static data associated with Library.
+It should be called at the end of execution to avoid memory leaks.
+@returns: This function returns 0 on success and -1 on error.
+*/
+int EXPORT_API usb_io_uninit(void)
+{
+    return 0;
+}
+
+
+/** Enumerate the devices.*/
+pusb_io_device_info_t EXPORT_API usb_io_get_device_list(void)
+{
+    struct enumctx_s ectx;
+    int ret;
+    memset(&ectx, 0, sizeof(ectx));
+    ret = usbhidEnumDevices(USB_CFG_VENDOR_ID, USB_CFG_DEVICE_ID,
+        (void*)&ectx,
+        enumfunc);
+
+    return (pusb_io_device_info_t)ectx.head;
+}
+
+
+/** Free an enumeration Linked List*/
+void USBRL_API usb_io_free_device_list(struct usb_io_device_info *dilist)
+{
+    struct usb_io_internal_s *p = (struct usb_io_internal_s *)dilist;
+
+    while ( p ) {
+        struct usb_io_internal_s *q = (struct usb_io_internal_s *)((pusb_io_device_info_t)p)->next;
+        if ( p->usbh && ((USBDEVHANDLE)(-1)) != p->usbh ) {
+            usbhidCloseDevice(p->usbh);
+            p->usbh = 0;
+        }
+        free(p);
+        p = q;
+    }
+
+    return;
+}
+
+
+#if 0 // not in orig API - do we want it?
+
 // Enum function for open one device by ID
 static
     int enumOpenfunc(USBDEVHANDLE usbh, void *context)
@@ -278,166 +349,121 @@ next:
     usbhidCloseDevice(usbh);
     return 1;
 }
-#endif
 
-// Public functions:
 
-/** Initialize the Library
-@returns: This function returns 0 on success and -1 on error.
+/** Open device by serial number
+serial_number == NULL is valid and means any one device.
+@return: This function returns a valid handle to the device on success or NULL on failure.
+Example: usb_relay_device_open_with_serial_number("abcde", 5)
 */
-int EXPORT_API usb_io_init(void)
-{
-    return 0;
-}
-
-/** Finalize the Library.
-This function frees all of the static data associated with Library.
-It should be called at the end of execution to avoid memory leaks.
-@returns: This function returns 0 on success and -1 on error.
-*/
-int EXPORT_API usb_io_uninit(void)
-{
-    return 0;
-}
-
-
-/** Enumerate the USB Relay Devices.*/
-pusb_io_device_info_t EXPORT_API usb_io_get_device_list(void)
+intptr_t USBRL_API usb_io_device_open_with_serial_number(const char *serial_number, unsigned len)
 {
     struct enumctx_s ectx;
     int ret;
+    struct usbrelay_internal_s *q;
     memset(&ectx, 0, sizeof(ectx));
+
+    if ( serial_number && len != USB_RELAY_ID_STR_LEN ) {
+        printerr("Specified invalid str id length: %u", len);
+        return (intptr_t)0;
+    }
+
+    q = ectx.head = calloc(1, sizeof(*ectx.head));
+    if ( !q )
+        return (intptr_t)0;
+
+    memcpy(q->idstr, serial_number, len);
+
     ret = usbhidEnumDevices(USB_CFG_VENDOR_ID, USB_CFG_DEVICE_ID,
         (void*)&ectx,
-        enumfunc);
+        enumOpenfunc);
+    if ( ret != 0 )
+        goto ret_err; // error during enum
 
-    return (pusb_io_device_info_t)ectx.head;
+    if ( ectx.numdevs == 0 || q->usbh == 0 ) {
+        goto ret_err; // not found
+    }
+
+    q->urdi.next = (void*)q; // mark this element as standalone
+    return (intptr_t)q;
+
+    ret_err:
+    free(q);
+    return (intptr_t)0;
+}
+#endif //-------------
+
+/** Open a  device
+@return: This function returns a valid handle to the device on success or NULL on failure.
+*/
+intptr_t EXPORT_API usb_io_open_device(struct usb_io_device_info *device_info)
+{
+    struct usb_io_internal_s *p = (struct usb_io_internal_s *)device_info;
+    if ( !device_info )
+        return 0;
+    if ( (uintptr_t)p->usbh == 0 || (uintptr_t)p->usbh == (uintptr_t)-1 )
+        return 0;
+    //$$$ validate more
+    return (uintptr_t)device_info;
 }
 
-
-/** Free an enumeration Linked List*/
-void USBRL_API usb_io_free_device_list(struct usb_io_device_info *dilist)
+/** Close a device*/
+void EXPORT_API usb_io_close_device(intptr_t hHandle)
 {
-    struct usb_io_internal_s *p = (struct usb_io_internal_s *)dilist;
+    struct usb_io_internal_s *p = (struct usb_io_internal_s *)hHandle;
+    if ( 0 == hHandle || ((intptr_t)-1) == hHandle )
+        return;
 
-    while ( p ) {
-        struct usb_io_internal_s *q = (struct usb_io_internal_s *)((pusb_io_device_info_t)p)->next;
-        if ( p->usbh && ((USBDEVHANDLE)(-1)) != p->usbh ) {
+    if ( (void*)(p->urdi.next) == (void*)p ) {
+        // This was made by usb_xxxx_device_open_with_serial_number() so free it now:
+        if ( p->usbh && ((intptr_t)-1) != (intptr_t)(p->usbh) ) {
             usbhidCloseDevice(p->usbh);
             p->usbh = 0;
         }
-        free(p);
-        p = q;
+        p->urdi.next = NULL;
+        free((void*)p);
     }
-
-    return;
+    // Else this can be in the live list, don't do anything.
 }
 
 
-#if 0 // not in orig API - do we want it?
-    /** Open device by serial number
-    serial_number == NULL is valid and means any one device.
-    @return: This function returns a valid handle to the device on success or NULL on failure.
-    Example: usb_relay_device_open_with_serial_number("abcde", 5)  */
-    intptr_t USBRL_API usb_io_device_open_with_serial_number(const char *serial_number, unsigned len)
-    {
-        struct enumctx_s ectx;
-        int ret;
-        struct usbrelay_internal_s *q;
-        memset(&ectx, 0, sizeof(ectx));
+int EXPORT_API usb_io_set_work_led_mode(intptr_t hHandle, enum work_led_mode led_mode)
+{
+    unsigned char buf[9];
+    struct usb_io_internal_s *p = (struct usb_io_internal_s *)hHandle;
 
-        if ( serial_number && len != USB_RELAY_ID_STR_LEN ) {
-            printerr("Specified invalid str id length: %u", len);
-            return (intptr_t)0;
-        }
-
-        q = ectx.head = calloc(1, sizeof(*ectx.head));
-        if ( !q )
-            return (intptr_t)0;
-
-        memcpy(q->idstr, serial_number, len);
-
-        ret = usbhidEnumDevices(USB_CFG_VENDOR_ID, USB_CFG_DEVICE_ID,
-            (void*)&ectx,
-            enumOpenfunc);
-        if ( ret != 0 )
-            goto ret_err; // error during enum
-
-        if ( ectx.numdevs == 0 || q->usbh == 0 ) {
-            goto ret_err; // not found
-        }
-
-        q->urdi.next = (void*)q; // mark this element as standalone
-        return (intptr_t)q;
-
-    ret_err:
-        free(q);
-        return (intptr_t)0;
-    }
-#endif //-------------
-
-    /** Open a  device
-    @return: This function returns a valid handle to the device on success or NULL on failure.
-    */
-    intptr_t EXPORT_API usb_io_open_device(struct usb_io_device_info *device_info)
-    {
-        struct usb_io_internal_s *p = (struct usb_io_internal_s *)device_info;
-        if ( !device_info )
-            return 0;
-        if ( (uintptr_t)p->usbh == 0 || (uintptr_t)p->usbh == (uintptr_t)-1 )
-            return 0;
-        //$$$ validate more
-        return (uintptr_t)device_info;
+    memset(buf, 0, sizeof(buf));
+    buf[1] = 0xF8;
+    if (led_mode) {
+        buf[2] = 1;
     }
 
-    /** Close a USB relay device*/
-    void EXPORT_API usb_io_close_device(intptr_t hHandle)
-    {
-        struct usb_io_internal_s *p = (struct usb_io_internal_s *)hHandle;
-        if ( 0 == hHandle || ((intptr_t)-1) == hHandle )
-            return;
+    return d16_write_rep(p->usbh, 0, buf);
+}
 
-        if ( (void*)(p->urdi.next) == (void*)p ) {
-            // This was made by usb_xxxx_device_open_with_serial_number() so free it now:
-            if ( p->usbh && ((intptr_t)-1) != (intptr_t)(p->usbh) ) {
-                usbhidCloseDevice(p->usbh);
-                p->usbh = 0;
-            }
-            p->urdi.next = NULL;
-            free((void*)p);
-        }
-        // Else this can be in the live list, don't do anything.
-    }
+int EXPORT_API usb_io_set_pin_mode(intptr_t hHandle,
+    unsigned pinIndex,
+    enum pin_mode mode,
+    enum input_pin_mode innerPullUp)
+{
+    return -1;
+}
+
+int EXPORT_API usb_io_write_output_pin_value(intptr_t hHandle, unsigned ouputPinIndex, enum pin_level level)
+{
+    return -1;
+}
 
 
-    int EXPORT_API usb_io_set_work_led_mode(intptr_t hHandle, enum work_led_mode led_mode)
-    {
-        return -1;
-    }
+int EXPORT_API usb_io_read_input_pin_value(intptr_t hHandle, unsigned pinIndex, unsigned *level)
+{
+    return -1;
+}
 
-    int EXPORT_API usb_io_set_pin_mode(intptr_t hHandle,
-        unsigned pinIndex,
-        enum pin_mode mode,
-        enum input_pin_mode innerPullUp)
-    {
-        return -1;
-    }
-
-    int EXPORT_API usb_io_write_output_pin_value(intptr_t hHandle, unsigned ouputPinIndex, enum pin_level level)
-    {
-        return -1;
-    }
-
-
-    int EXPORT_API usb_io_read_input_pin_value(intptr_t hHandle, unsigned pinIndex, unsigned *level)
-    {
-        return -1;
-    }
-
-    int EXPORT_API usb_io_get_all_pin_info(intptr_t hHandle, struct pin_info info[USB_IO16_MAX_PIN_NUM])
-    {
-        return -1;
-    }
+int EXPORT_API usb_io_get_all_pin_info(intptr_t hHandle, struct pin_info info[USB_IO16_MAX_PIN_NUM])
+{
+    return -1;
+}
 
 
 /************ Added ****************/
@@ -473,7 +499,6 @@ intptr_t EXPORT_API usb_io_device_get_id_string(intptr_t ptr_device_info)
         return 0;
     return (intptr_t)(void const *)((pusb_io_device_info_t)ptr_device_info)->serial_number;
 }
-
 
 #ifdef __cplusplus
 }
